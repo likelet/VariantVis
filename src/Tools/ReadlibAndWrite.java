7f7a4d9942c5dd0a98ef8f64ab704858c9cb1170
Zinky-i7 <Zinky-i7@172.18.119.19>
2015-12-31 21:52:52 +0800

    initialized version

---
+++ src/Tools/ReadlibAndWrite.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tools;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Zinky-i7
+ */
+public class ReadlibAndWrite {
+
+    //wright external js
+    public static String WriteJS(String jsfile) throws IOException {
+        //src\\resources\\Highcharts.js
+         StringBuilder builder = new StringBuilder();
+        builder.append("<script type=\"text/javascript\">\n");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(jsfile)));
+            //FileWriter fw = new FileWriter(outputFile);
+
+            while (br.ready()) {
+                builder.append(br.readLine().trim());
+            }
+            //fw.flush();
+            br.close();
+        } catch (FileNotFoundException ex) {
+
+        }
+        builder.append("\n</script>\n");
+        return builder.toString();
+    }
+
+    //write external css
+    public static String WriteCSS(String cssfile) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        builder.append("<script type=\"text/css\">\n");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(cssfile)));
+            //FileWriter fw = new FileWriter(outputFile);
+
+            while (br.ready()) {
+                builder.append(br.readLine().trim());
+            }
+            //fw.flush();
+            br.close();
+        } catch (FileNotFoundException ex) {
+
+        }
+        builder.append("\n</script>\n");
+        return builder.toString();
+    }
+}
---
+++ src/WriteReport/HeaderString.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package WriteReport;
+
+/**
+ *
+ * @author Zinky-i7
+ */
+public class HeaderString {
+    StringBuilder builder = new StringBuilder();
+
+    public HeaderString() {
+       builder.append("<head>");
+       
+       builder.append("</head>");
+    }
+    
+}
