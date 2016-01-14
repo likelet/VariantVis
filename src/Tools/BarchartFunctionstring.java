3628c9266aed563e1da867dfb0c81b5de4d46a0a
zhaoqi <zhaoqi@zhaoqideair.lan>
2016-01-14 13:17:36 +0800

    add single class line chart

---
+++ src/Tools/BarchartFunctionstring.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tools;
+
+/**
+ * <p>BarchartFunctionstring</p>
+ * <p>Created on 2016-1-8 0:11:03</p>
+ * <p>Author Email: zhaoqi3@mail2.sysu.edu.cn</p>
+ * @author ZHAO Qi
+ * @date 2016-1-8 0:11:03
+ * @version java 1.6.0
+ * @version 
+ */
+public class BarchartFunctionstring extends FigureClass {
+
+}
--- src/WriteReport/WriteHtml.java
+++ src/WriteReport/WriteHtml.java
@@ -9,6 +9,7 @@ import Tools.PiechartFunctionstring;
 import Tools.ReadlibAndWrite;
 import Tools.SingleClassLinechartFunctionstring;
 import Tools.StringCoverter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -114,7 +115,46 @@ public class WriteHtml {
         return str;
     }
 
+    public static void openExplorer(String url){
+            if(java.awt.Desktop.isDesktopSupported()){
+            try{
+                //创建一个URI实例,注意不是URL
+                java.net.URI uri=java.net.URI.create(url);
+                //获取当前系统桌面扩展
+                java.awt.Desktop dp=java.awt.Desktop.getDesktop();
+                //判断系统桌面是否支持要执行的功能
+                if(dp.isSupported(java.awt.Desktop.Action.BROWSE)){
+                    //获取系统默认浏览器打开链接
+                    dp.browse(uri);
+                }
+            }catch(java.lang.NullPointerException e){
+                //此为uri为空时抛出异常
+            }catch(java.io.IOException e){
+                //此为无法获取系统默认浏览器
+            }
+        }
+    }
+     public static void openExplorerLocalFile(String filename){
+            if(java.awt.Desktop.isDesktopSupported()){
+            try{
+                //创建一个URI实例,注意不是URL
+                java.net.URI uri=java.net.URI.create("file://"+new File(filename).getAbsolutePath());
+                //获取当前系统桌面扩展
+                java.awt.Desktop dp=java.awt.Desktop.getDesktop();
+                //判断系统桌面是否支持要执行的功能
+                if(dp.isSupported(java.awt.Desktop.Action.BROWSE)){
+                    //获取系统默认浏览器打开链接
+                    dp.browse(uri);
+                }
+            }catch(java.lang.NullPointerException e){
+                //此为uri为空时抛出异常
+            }catch(java.io.IOException e){
+                //此为无法获取系统默认浏览器
+            }
+        }
+    }
     public static void main(String[] args) {
         new WriteHtml("test.html");
+        WriteHtml.openExplorer("file://"+new File("test.html").getAbsolutePath());
     }
 }
